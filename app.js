21be7c80fbdbeca663b3b8ace0d6470a09eca65a
abspen1 <abspencer2097@gmail.com>
2020-10-10 20:45:59 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+require('dotenv').config({ path: '.env' })
+
+const pass = process.env.COD
+const email = process.env.EMAIL
+const user = process.env.USER
+const API = require('call-of-duty-api')({ platform: "battle" });
+// API.login(email, pass).then(data => {
+//     //I want Warzone Data
+//     API.MWBattleData(user, API.platforms.psn).then(data => {
+//     console.log(data);  // see output
+//     }).catch(err => {
+//         console.log(err);
+//     });
+// }).catch(err => {
+//     console.log(err)
+// });
+
+API.login(email, pass).then(data => {
+    //I want Warzone Data
+    API.MWcombatmp(user, API.platforms.psn).then(data => {
+    console.log(data);  // see output
+    }).catch(err => {
+        console.log(err);
+    });
+}).catch(err => {
+    console.log(err)
+});
