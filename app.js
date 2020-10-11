38f0e8d20fff112bcc14527d5e1dd271929465a2
abspen1 <abspencer2097@gmail.com>
2020-10-10 23:25:40 -0700

    adding docker

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 require('dotenv').config({ path: '.env' })
-require('axios')
+const axios = require('axios')
 
+// Global Variables
 const pass = process.env.COD
 const email = process.env.EMAIL
-const user = process.env.USER
+const auth = process.env.SECRET
+const BASE_URL = process.env.BASE_URL
+var postData = new Object()
+postData.Auth = auth
+let jsonData
+
+//Call of Duty API functions
 const API = require('call-of-duty-api')({ platform: "battle" });
 // API.login(email, pass).then(data => {
 //     //I want Warzone Data
@@ -18,11 +25,54 @@ const API = require('call-of-duty-api')({ platform: "battle" });
 
 API.login(email, pass).then(data => {
     //I want Warzone Data
-    API.MWcombatmp(user, API.platforms.psn).then(data => {
-    console.log(data);  // see output
+    // API.MWmp("pr1vateryan2097", API.platforms.psn).then(data => {
+    // console.log(data);  // see output
+    // }).catch(err => {
+    //     console.log(err);
+    // });
+    // API.MWFullMatchInfomp("13287832752335928754", API.platforms.psn).then(data => {
+    // console.log(data);  // see output
+    // }).catch(err => {
+    //     console.log(err);
+    // });
+    // API.MWMapList(API.platforms.psn).then(data => {
+    // console.log(data);  // see output
+    // }).catch(err => {
+    //     console.log(err);
+    // });
+    API.MWweeklystats("pr1vateryan2097", API.platforms.psn).then(data => {
+    console.log(data.mp.all.properties);  // see output
+    let kills = data.mp.all.properties.kills
+    let kd = data.mp.all.properties.kdRatio
+    let wl = data.mp.all.properties.wlRatio
+    let message = `pr1vateryan2097 Call of Duty stats for the past week: \nKills: ${kills}\nKD: ${kd}\nWin/Loss: ${wl}`
+    postData.Message = message
+    jsonData = JSON.stringify(postData)
+    postTweet(jsonData)
     }).catch(err => {
         console.log(err);
     });
 }).catch(err => {
     console.log(err)
 });
+
+// Axios back-end post request function
+const postTweet = async () => {
+    try {
+        axios({
+          url: `${BASE_URL}/austinapi/go-tweet`,
+          method: 'post',
+          data: jsonData,
+        })
+          .then(function (response) {
+            // your action after success
+            console.log(response.data)
+          })
+          .catch(function (error) {
+            // your action on error success
+            console.log(error)
+          })
+    } catch (e) {
+        console.error(e)
+    }
+}
